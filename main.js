629772a9cf4d94c5cf6e1886804082fd130f8f9e
Sebastian <sebastianrodriguezds@gmail.com>
2023-11-10 23:21:18 +0000

    responsive.design

--- main.js
+++ main.js
@@ -21,3 +21,54 @@ document.querySelectorAll('.nav-links a').forEach(link =>{
   })
 })
 
+const tOriginal = "ON THE ROUTE TO BEING A WEB DEVELOPER,";
+const tSecondary = " SEBASTIAN RODRIGUEZ.";
+const msj = document.getElementById("msj");
+const durationDelete = 100;
+const durationWrite = 1000;
+
+function deleteAndWrite() {
+  let text = msj.innerText;
+  let long = text.length;
+
+  if (long > 0) {
+    msj.innerText = text.substring (0, long - 1);
+    setTimeout (deleteAndWrite , durationDelete);
+  }else {
+    setTimeout( writeText , durationWrite);
+  }
+}
+
+function writeText() {
+  let text = tOriginal + tSecondary;
+  let long = msj.innerText.length;
+
+  if(long < text.length) {
+    msj.innerText = text.substring (0, long + 1);
+    setTimeout(writeText, durationDelete);
+  }else {
+    setTimeout (deleteAndWrite, durationWrite);
+  }
+}
+deleteAndWrite();
+
+document.getElementById("icon-menu").
+  addEventListener('click', showMenu);
+function showMenu() {
+  document.getElementById('move-content').classList.toggle('move-container-all');
+
+  document.getElementById('show-menu').classList.toggle('show-lateral');
+}
+
+// -------CLOSE LATERAL------
+
+document.addEventListener('click', function (event){
+  if (!event.target.closest('#icon-menu') && !event.target.closest('.nav.links')){
+    var lateral = document.querySelector('.show-lateral');
+
+    if(lateral) {
+      lateral.classList.remove('show-lateral');
+    }
+  }
+});
+
